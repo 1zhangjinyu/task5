e1c049838063ff6d78dff81ac28cd3db2bad9a27
zhangjinyu <869554973@qq.com>
2020-11-09 17:49:33 +0800

    5

---
+++ build/precache-manifest.3b0539595137e8a1ebbff598134ad4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6365b8dde15e9fd0e12b965d31d6e0c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce478fa9aa88962240b",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "c49d401e3e4aabe69ce0",
+    "url": "/static/css/main.9880fa0d.chunk.css"
+  },
+  {
+    "revision": "fce478fa9aa88962240b",
+    "url": "/static/js/2.5fd31a73.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.5fd31a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49d401e3e4aabe69ce0",
+    "url": "/static/js/main.2a5c87d9.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
